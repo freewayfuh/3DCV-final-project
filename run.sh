eaa6cfcbdee5aaf87fb8b8fb4a29c4cdaa8c7ef1
小威威寶寶 <46265607+freewayfuh@users.noreply.github.com>
2022-12-26 23:22:55 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ do
     for INDEX in {0..59}
     do
         echo python ply2image.py --ply_file result/$METHOD/morph_ply/$INDEX.ply --output result/$METHOD/morph_png/$INDEX.png --cam_param cam.json
-        # python ply2image.py --ply_file result/$METHOD/morph_ply/$INDEX.ply --output result/$METHOD/morph_png/$INDEX.png --cam_param cam.json
+        python ply2image.py --ply_file result/$METHOD/morph_ply/$INDEX.ply --output result/$METHOD/morph_png/$INDEX.png --cam_param cam.json
     done
     echo python image2video.py --png_folder result/$METHOD/morph_png --output result/$METHOD/demo.mp4
-    # python image2video.py --png_folder result/$METHOD/morph_png --output result/$METHOD/demo.mp4
+    python image2video.py --png_folder result/$METHOD/morph_png --output result/$METHOD/demo.mp4
 done
